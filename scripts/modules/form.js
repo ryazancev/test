739af4f728fd2e9bcf2181dd2625e462ad23e981
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2021-03-03 23:25:58 +0300

    переписал скрипт form

--- scripts/modules/form.js
+++ scripts/modules/form.js
@@ -1,14 +1,10 @@
 const formOrder = () => {
 	const form = document.querySelector('.order__form'),
-		inputName = document.querySelector('.input-name'),
-		inputTel = document.querySelector('.input-tel'),
 		helpText = document.createElement('div'),
 		orderLabelName = document.querySelector('.order__label--name'),
 		orderLabelTel = document.querySelector('.order__label--tel');
 
 	helpText.classList.add('helpText');
-	
-	
 
 	form.addEventListener('input', event => {
 		const target = event.target;
@@ -21,24 +17,20 @@ const formOrder = () => {
 			target.value = target.value.replace(/\D/, '');
 		}
 	})
-	
-	inputName.addEventListener('focus', () => {
-		helpText.textContent = 'Пожалуйста введите Имя и Фамилию через пробел. Например, Иван Иванов';
-		orderLabelName.append(helpText);
-	});
 
-	inputName.addEventListener('blur', () => {
-		helpText.remove();
-	})
+	form.addEventListener('focus', (event) => {
+		const target = event.target
 
-	inputTel.addEventListener('focus', () => {
-		helpText.textContent = 'Пожалуйста введите номер телефона, начиная с цифры 8';
-		orderLabelTel.append(helpText);
-	});
+		if (target.type === 'text') {
+			helpText.textContent = 'Пожалуйста введите Имя и Фамилию через пробел. Например, Иван Иванов';
+			orderLabelName.append(helpText);
+		}
 
-	inputTel.addEventListener('blur', () => {
-		helpText.remove();
-	})
+		if (target.type === 'tel') {
+			helpText.textContent = 'Пожалуйста введите номер телефона, начиная с цифры 8';
+			orderLabelTel.append(helpText);
+		}
+	}, true)
 
 };
 
